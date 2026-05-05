fc6ebf4e9699e6f6d29cc7c2baaa31da7e42c03a
Krish <krishadhikari@gmail.com>
2026-05-05 13:17:47 +0545

    edit button

--- learnhub/core/urls.py
+++ learnhub/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('edit/<int:id>/', views.edit_note, name='edit_note'),
 ]
--- learnhub/core/views.py
+++ learnhub/core/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
+from .forms import NoteForm
 
 
 # Create your views here.
 
 
 
-
 def home(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm()
+
     notes = Note.objects.all()
+
     return render(request, 'core/home.html', {
-        'notes': notes
+        'notes': notes,
+        'form': form
+    })
+def edit_note(request, id):
+    note = get_object_or_404(Note, id=id)
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, 'core/edit_note.html', {
+        'form': form
     })
